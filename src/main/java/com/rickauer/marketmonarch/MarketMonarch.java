05bcb62efc2879d7c7b0ac8b63398701e39696cd
Stefan Rickauer <stefanrick@gmx.de>
2024-08-26 21:42:28 +0200

    Added logger
    Signed-off-by: Stefan Rickauer <stefanrick@gmx.de>

--- src/main/java/com/rickauer/marketmonarch/MarketMonarch.java
+++ src/main/java/com/rickauer/marketmonarch/MarketMonarch.java
@@ -1,13 +1,21 @@
 package com.rickauer.marketmonarch;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class MarketMonarch {
 
 	private static final String PROGRAM	= "MarketMonarch";
 	private static final String VERSION	= "0.01";
 	
+	private static Logger marketMonarchLogger = LogManager.getLogger(MarketMonarch.class);
+	
 	public static void main(String[] args) {
 		try {
 			// TODO: Logger is supposed to log to console as well as log file.
+			// TODO: Reviese log4j2.xml
+			marketMonarchLogger.info("Starting " + PROGRAM + " (version " + VERSION + ")...");
 			System.out.println("Starting " + PROGRAM + " (version " + VERSION + ")...");
 		} catch (Exception e) {
 			e.printStackTrace();
