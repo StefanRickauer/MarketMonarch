27a4609182e126760c3e916336c4b5162da3700b
Stefan Rickauer <stefanrick@gmx.de>
2024-08-28 23:12:01 +0200

    Deleted artifact

--- src/main/java/com/rickauer/marketmonarch/MarketMonarch.java
+++ src/main/java/com/rickauer/marketmonarch/MarketMonarch.java
@@ -15,15 +15,10 @@ public class MarketMonarch {
 		try {
 			// TODO: Reviese log4j2.xml
 			marketMonarchLogger.info("Starting " + PROGRAM + " (version " + VERSION + ")...");
-			test();
 		} catch (Exception e) {
 			// Workaround because usage of e will throw exception.
 			String stackTrace = ExceptionUtils.getStackTrace(e);
 			marketMonarchLogger.error(stackTrace);
 		}
-	}
-	
-	private static void test() {
-		int a = 5 / 0;
 	}
 }
