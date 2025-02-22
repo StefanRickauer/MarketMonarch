24e62b696778f9ea0cac1f562bb3955554679b0b
Stefan Rickauer <stefanrick@gmx.de>
2025-02-22 18:01:09 +0100

    Code für Scanner-Anfrage aus Main-Methode in separate Methode ausgelagert.
    Signed-off-by: Stefan Rickauer <stefanrick@gmx.de>

--- src/main/java/com/rickauer/marketmonarch/MarketMonarch.java
+++ src/main/java/com/rickauer/marketmonarch/MarketMonarch.java
@@ -29,11 +29,11 @@ import org.apache.commons.lang3.exception.*;
 
 public final class MarketMonarch {
 
-	private static final String PROGRAM	= "MarketMonarch";
-	private static final String VERSION	= "0.1";
+	private static final String PROGRAM = "MarketMonarch";
+	private static final String VERSION = "0.1";
 
 	private static Logger _marketMonarchLogger = LogManager.getLogger(MarketMonarch.class.getName());
-	
+
 	private static HealthChecker _healthChecker = new HealthChecker();
 	public static ApiKeyAccess _apiAccess;
 	private static FinancialDataAccess _finAccess;
@@ -44,55 +44,38 @@ public final class MarketMonarch {
 	private static Object _sharedLock;
 	private static ScannerResponse _responses;
 
-	
 	static {
 		_sharedLock = new Object();
 		_responses = new ScannerResponse(_sharedLock);
-		
+
 		_ibController = new InteractiveBrokersApiController(_responses);
 
 		ConfigReader.INSTANCE.initializeConfigReader();
-		
+
 		_apiAccess = new ApiKeyAccess(ConfigReader.INSTANCE.getUrlAPIKey(), ConfigReader.INSTANCE.getUsername(), ConfigReader.INSTANCE.getPassword());
 		_finAccess = new FinancialDataAccess(ConfigReader.INSTANCE.getUrlAPIKey(), ConfigReader.INSTANCE.getUsername(), ConfigReader.INSTANCE.getPassword());
 		_mailtrapService = new MailtrapServiceConnector("mailtrap", _apiAccess.executeSqlQueryAndGetFirstResultAsString("SELECT token FROM credentials where provider = 'mailtrap'", "token"));
 		_fmp = new FmpConnector("fmp", _apiAccess.executeSqlQueryAndGetFirstResultAsString("SELECT token FROM credentials where provider = 'FMP'", "token"));
 		_alphaVantage = new AlphaVantageConnector("alphavantageapi", _apiAccess.executeSqlQueryAndGetFirstResultAsString("SELECT token FROM credentials where provider = 'alphavantage'", "token"));
 		ConfigReader.INSTANCE.flushDatabaseConnectionEssentials();
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		try {
 			_marketMonarchLogger.info("Starting " + PROGRAM + " (version " + VERSION + ").");
 			ensureOperationalReadiness();
+			scanMarketAndSaveResult();
 			
-			// Get market scanner and save response
-			ScannerSubscription subscription = new ScannerSubscription();
-			subscription.instrument("STK");
-			subscription.locationCode("STK.US.MAJOR");
-			subscription.scanCode("TOP_PERC_GAIN"); 
-			List<TagValue> filterTagValues = new LinkedList<>();
-			filterTagValues.add(new TagValue("priceAbove", "2"));
-			filterTagValues.add(new TagValue("priceBelow", "20"));
-			_ibController.getSocket().reqScannerSubscription(_ibController.getRequestId(), subscription, null, filterTagValues);
+			// request historical data for all results (5 minute candles)
+			// calculate percentage gain; filter out all stocks < 10% gain
+			// calculate RVOL for 30 minute intervals
+			// request company share float; filter out all stocks > 20.000.000 
 			
-			synchronized(_sharedLock) {
-				try {
-					_sharedLock.wait();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			
-			for (Map.Entry<Integer, Contract> entry : _responses.getRankings().entrySet()) {
-				System.out.println("Rank: " + entry.getKey() + ", Symbol: " + entry.getValue().symbol());
-			}
-			// 
-			
-			// Query other credentials
 			// Make money
-			// Docs: Orders are submitted via the EClient.placeOrder method. From the snippet below, note how a variable holding the nextValidId is incremented automatically.
+			// Docs: Orders are submitted via the EClient.placeOrder method. From the
+			// snippet below, note how a variable holding the nextValidId is incremented
+			// automatically.
 		} catch (Throwable t) {
 			// Workaround because usage of t will throw exception.
 			String stackTrace = ExceptionUtils.getStackTrace(t);
@@ -102,6 +85,7 @@ public final class MarketMonarch {
 			_finAccess.close();
 		}
 	}
+
 	
 	private static void ensureOperationalReadiness() {
 		_marketMonarchLogger.info("Preparing for operational readiness check...");
@@ -113,13 +97,43 @@ public final class MarketMonarch {
 		_healthChecker.add(_fmp);
 		_healthChecker.add(_alphaVantage);
 		_healthChecker.add(_ibController);
-		
+
 		_marketMonarchLogger.info("Checking operational readiness...");
 		_healthChecker.runHealthCheck();
 		_marketMonarchLogger.info("Checked operational readiness.");
-		
+
 		_marketMonarchLogger.info("Evaluating check results...");
 		_healthChecker.analyseCheckResults();
 		_marketMonarchLogger.info("Evaluated check results.");
+	}
+
+	
+	private static void scanMarketAndSaveResult() {
+	
+		_marketMonarchLogger.info("Setting up market scanner and requesting scan results...");
+		
+		ScannerSubscription subscription = new ScannerSubscription();
+		subscription.instrument("STK");
+		subscription.locationCode("STK.US.MAJOR");
+		subscription.scanCode("TOP_PERC_GAIN");
+		List<TagValue> filterTagValues = new LinkedList<>();
+		filterTagValues.add(new TagValue("priceAbove", "2"));
+		filterTagValues.add(new TagValue("priceBelow", "20"));
+		_ibController.getSocket().reqScannerSubscription(_ibController.getRequestId(), subscription, null, filterTagValues);
+
+		synchronized (_sharedLock) {
+			try {
+				_sharedLock.wait();
+			} catch (InterruptedException e) {
+				throw new RuntimeException("Error scanning market.", e);
+			}
+		}
+		
+		_marketMonarchLogger.info("Set up market scanner and received scan results.");
+
+		// DEBUG only!
+		for (Map.Entry<Integer, Contract> entry : _responses.getRankings().entrySet()) {
+			System.out.println("Rank: " + entry.getKey() + ", Symbol: " + entry.getValue().symbol());
+		}
 	}
 }
