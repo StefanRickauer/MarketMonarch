c356ef901ad35dce061c1bd5a3623fee2409dc8a
Stefan Rickauer <stefanrick@gmx.de>
2025-02-25 21:29:12 +0100

    Enum hinzugefügt, dass die verschiedenen Tageszeiten als MinuteOfDay-Werte speichert.
    Signed-off-by: Stefan Rickauer <stefanrick@gmx.de>

---
+++ src/main/java/com/rickauer/marketmonarch/utils/TradingTime.java
@@ -0,0 +1,29 @@
+package com.rickauer.marketmonarch.utils;
+
+public enum TradingTime {
+
+	NINE_THIRTY(570),
+	TEN(600),
+	TEN_THIRTY(630),
+	ELEVEN(660),
+	ELEVEN_THIRTY(690),
+	TWELVE(720),
+	TWELVE_THIRTY(750),
+	THIRTEEN(780),
+	THIRTEEN_THIRTY(810),
+	FOURTEEN(840),
+	FOURTEEN_THIRTY(870),
+	FIFTEEN(900),
+	FIFTEEN_THIRTY(930),
+	SIXTEEN(960);
+	
+	private int _minuteOfDay;
+	
+	private TradingTime(int minuteOfDay) {
+		_minuteOfDay = minuteOfDay;
+	}
+	
+	public int toMinutes() {
+		return _minuteOfDay;
+	}
+}
