c8b5ad1b1dfd334513fd076007b6a1cb3a155d07
Stefan Rickauer <stefanrick@gmx.de>
2025-03-23 14:40:14 +0100

    FmpRequestController an die Erweiterung in FmpServiceRequest angepasst. Methoden zur Abfrage aller Float sowie zum Filtern aller Floats nach einem spezifischen Symbol hinzugefügt.
    Signed-off-by: Stefan Rickauer <stefanrick@gmx.de>

--- src/main/java/com/rickauer/marketmonarch/api/controller/FmpRequestController.java
+++ src/main/java/com/rickauer/marketmonarch/api/controller/FmpRequestController.java
@@ -17,38 +17,88 @@ public class FmpRequestController {
 
 	private static Logger _fmpRequestLogger = LogManager.getLogger(FmpRequestController.class.getName());
 	private String _token;
-	
+	private FmpServiceRequest _serviceRequest;
+
 	public FmpRequestController(String token) {
 		_token = token;
+		_serviceRequest = FmpServiceRequest.COMPANY_SHARE_FLOAT;
 	}
-	
+
+	public FmpRequestController(String token, FmpServiceRequest serviceRequest) {
+		_token = token;
+		_serviceRequest = serviceRequest;
+	}
+
+	@Deprecated
 	public Long requestCompanyShareFloat(String symbol) {
-		String request = String.format(FmpServiceRequest.COMPANY_SHARE_FLOAT.getServiceRequest(), symbol, _token);
-		
-		return requestFloat(request, symbol); 
+		String request = String.format(_serviceRequest.getServiceRequest(), symbol, _token);
+
+		return requestFloat(request, symbol);
 	}
-	
+
 	private Long requestFloat(String request, String symbol) {
 		_fmpRequestLogger.info("Requesting company share float for symbol: '" + symbol + "'.");
 		String response = RequestHandler.sendRequest(request);
-		
+
 		Long companyShareFloat = -1L;
-		
+
 		if (response.contains("Exclusive Endpoint") || response.equals("")) {
 			_fmpRequestLogger.error("Invalid request. Received:\n'" + response + "'.");
 			return companyShareFloat;
 		}
-		
+
 		try {
-			Object responseObject = new JSONParser().parse(response.toString());
+			Object responseObject = new JSONParser().parse(response);
 			JSONArray array = (JSONArray) responseObject;
 			JSONObject dataObject = (JSONObject) array.get(0);
-			
+
 			companyShareFloat = (Long) dataObject.get("floatShares");
 
 		} catch (Exception e) {
 			_fmpRequestLogger.error(e.getMessage());
 		}
+		return companyShareFloat;
+	}
+
+	public String requestAllShareFloat() {
+		_fmpRequestLogger.info("Requesting all shares float...");
+		String request = String.format(_serviceRequest.getServiceRequest(), _token);
+		String response = "";
+		
+		response = RequestHandler.sendRequest(request);
+		
+		if (response.contains("Exclusive Endpoint") || response.equals("")) {
+			_fmpRequestLogger.error("Invalid request. Received:\n'" + response + "'.");
+			return "";
+		}
+		
+		return response;
+	}
+
+	public Long filterAllFloatsForSymbol(String allFloats, String symbolToSearchFor) {
+		_fmpRequestLogger.info("Requesting all shares float and filtering for symbol: " + symbolToSearchFor);
+		String symbol = "";
+
+		Long companyShareFloat = -1L;
+
+		try {
+			Object responseObject = new JSONParser().parse(allFloats);
+			JSONArray array = (JSONArray) responseObject;
+
+			for (int i = 0; i < array.size(); i++) {
+
+				JSONObject dataObject = (JSONObject) array.get(i);
+				symbol = (String) dataObject.get("symbol");
+				
+				if (symbol.equals(symbolToSearchFor)) {
+					companyShareFloat = (Long) dataObject.get("floatShares");
+				}
+			}
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
 		return companyShareFloat;
 	}
 }
