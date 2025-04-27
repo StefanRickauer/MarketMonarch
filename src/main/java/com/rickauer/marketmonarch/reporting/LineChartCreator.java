dc5e18573467158d71a5266f2ec5b95e47135173
Stefan Rickauer <stefanrick@gmx.de>
2025-04-27 14:57:12 +0200

    Plotting verbessert und Testklasse angepasst.
    Signed-off-by: Stefan Rickauer <stefanrick@gmx.de>

--- src/main/java/com/rickauer/marketmonarch/reporting/LineChartCreator.java
+++ src/main/java/com/rickauer/marketmonarch/reporting/LineChartCreator.java
@@ -1,16 +1,34 @@
 package com.rickauer.marketmonarch.reporting;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.annotations.XYDrawableAnnotation;
+import org.jfree.chart.annotations.XYTextAnnotation;
+import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.ui.TextAnchor;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
+import com.rickauer.marketmonarch.data.CandleStick;
 import com.rickauer.marketmonarch.utils.FileSupplier;
 
 public final class LineChartCreator {
@@ -26,17 +44,116 @@ public final class LineChartCreator {
 		tradingData = new DefaultCategoryDataset();
 	}
 	
-	public void createLineGraphAndSaveFile() {
+	public void createLineGraphAndSaveFile(List<CandleStick> tradedStock, double stopLoss) {
 		
-		lineGraphCreatorLogger.info("Creating line graph.");
+		Font font = new Font("Arial", Font.BOLD, 12);
 		
-		JFreeChart lineChartObject = ChartFactory.createLineChart("Invested and Total Capital, Return", "Trading Day", "Euro", tradingData, PlotOrientation.VERTICAL, true, true, false);
+		XYSeries series = new XYSeries("Trade");
+		for (CandleStick stick : tradedStock) {
+			series.add(stick.getInstant().toEpochMilli(), stick.getClose());
+		}
 		
-		int width = 1280; 
-		int height = 960;
+		XYSeriesCollection dataset = new XYSeriesCollection(series);
+		JFreeChart chart = ChartFactory.createTimeSeriesChart(
+				"Linienchart - 5 Sekunden Zeitintervall",
+				"Zeit",
+				"Preis pro Aktie",
+				dataset,
+				false, true, false
+				);
+		
+		XYPlot plot = chart.getXYPlot();
+		
+		DateAxis axis = (DateAxis) plot.getDomainAxis();
+		axis.setDateFormatOverride(new SimpleDateFormat("HH:mm:ss"));
+		
+		XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer(true, false);
+		renderer.setSeriesPaint(0, new Color(40, 100, 200));
+		renderer.setSeriesStroke(0, new BasicStroke(1.0f));
+		plot.setRenderer(renderer);
+		
+		; // replace value with acutal buy time and buy price, sell time and sell price
+		Date buyTime = Date.from(tradedStock.get(10).getInstant());
+		double buyPrice = tradedStock.get(10).getClose();
+		Date sellTime = Date.from(tradedStock.get(tradedStock.size() - 10).getInstant());
+		double sellPrice = tradedStock.get(20).getClose();
+		
+		XYDrawableAnnotation buyMarker = new XYDrawableAnnotation(
+				buyTime.getTime(),
+				buyPrice,
+				14,
+				14,
+				(Graphics2D g2, Rectangle2D area) -> {
+					g2.setColor(Color.GRAY.brighter());
+					g2.fill(new Ellipse2D.Double(area.getX(), area.getY(), area.getWidth(), area.getHeight()));
+					g2.setColor(Color.BLACK);
+					g2.draw(new Ellipse2D.Double(area.getX(), area.getY(), area.getWidth(), area.getHeight()));
+				});
+		
+		plot.addAnnotation(buyMarker);
+		
+		XYTextAnnotation buyText = new XYTextAnnotation("Kauf", buyTime.getTime(), buyPrice + 0.1);
+		buyText.setFont(font);
+		buyText.setPaint(Color.GRAY.darker());
+		plot.addAnnotation(buyText);
+
+		Color sellColor = (sellPrice - buyPrice > 0) ? Color.GREEN : Color.RED;
+		
+		XYDrawableAnnotation sellMarker = new XYDrawableAnnotation(
+				sellTime.getTime(),
+				sellPrice,
+				14,
+				14,
+				(Graphics2D g2, Rectangle2D area) -> {
+					g2.setColor(sellColor.brighter());
+					g2.fill(new Ellipse2D.Double(area.getX(), area.getY(), area.getWidth(), area.getHeight()));
+					g2.setColor(Color.BLACK);
+					g2.draw(new Ellipse2D.Double(area.getX(), area.getY(), area.getWidth(), area.getHeight()));
+				});
+		plot.addAnnotation(sellMarker);
+		
+		XYTextAnnotation sellText = new XYTextAnnotation("Verkauf", sellTime.getTime(), sellPrice + 0.1);
+		sellText.setFont(font);
+		sellText.setPaint(sellColor.darker());
+		plot.addAnnotation(sellText);
+		
+		XYSeries stopLossSeries = new XYSeries("Stop Loss");
+		stopLossSeries.add(buyTime.getTime(), stopLoss);
+		stopLossSeries.add(sellTime.getTime(), stopLoss);
+		
+		dataset.addSeries(stopLossSeries);
+		XYTextAnnotation stopLossLabel = new XYTextAnnotation(
+				"Stop Loss", 
+				buyTime.getTime() + (sellTime.getTime() - buyTime.getTime()) / 2,
+				stopLoss + 0.02);
+		stopLossLabel.setFont(font);
+		stopLossLabel.setPaint(Color.RED);
+		stopLossLabel.setTextAnchor(TextAnchor.BOTTOM_CENTER);
+		renderer.setSeriesStroke(1, new BasicStroke(
+				1.0f,
+				BasicStroke.CAP_BUTT,
+				BasicStroke.JOIN_BEVEL, 
+				0,
+				new float[] {5.0f, 5.0f },
+				0.0f));
+		plot.addAnnotation(stopLossLabel);
+		
+		plot.setDomainGridlineStroke(new BasicStroke(0.5f));
+		plot.setRangeGridlineStroke(new BasicStroke(0.5f));
+		chart.setBackgroundPaint(Color.WHITE);
+		plot.setBackgroundPaint(Color.WHITE);
+		plot.setDomainGridlinePaint(new Color(220, 220, 220));
+		plot.setRangeGridlinePaint(new Color(220, 220, 220));
+		
+		chart.setBorderVisible(false);
+		plot.setOutlineVisible(false);
+		
 		File lineGraph = new File(LINECHART);
+		int chartWidth = 1200;
+		int chartHeight = 600;
+		
 		try {
-			ChartUtils.saveChartAsJPEG(lineGraph, lineChartObject, width, height);
+			ChartUtils.saveChartAsJPEG(lineGraph, chart, chartWidth, chartHeight);
 			lineGraphCreatorLogger.info("Line graph saved to: " + LINECHART);
 		} catch (IOException e) {
 			throw new RuntimeException("Could not save line graph.", e);
--- src/test/java/com/rickauer/marketmonarch/reporting/LineChartCreatorTest.java
+++ src/test/java/com/rickauer/marketmonarch/reporting/LineChartCreatorTest.java
@@ -5,26 +5,53 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import com.ib.client.Decimal;
+import com.rickauer.marketmonarch.data.CandleStick;
 import com.rickauer.marketmonarch.utils.FileSupplier;
+import com.rickauer.marketmonarch.utils.StockUtils;
 
 public class LineChartCreatorTest {
 
 	private File lineChart = new File(LineChartCreator.LINECHART);
+	private static List<CandleStick> mockData;
+	private static double stopLoss;
 	
 	@BeforeAll
 	public static void initializeTestData() {
 		FileSupplier.createFolder(FileSupplier.getTemporaryFolder());
+		mockData = new ArrayList<>();
+		ZonedDateTime time = ZonedDateTime.parse("2025-04-25T11:50:05+02:00[Europe/Berlin]");
+		for (int i = 300; i >= 0; i--) {
+			
+			mockData.add(new CandleStick(
+					time.minusSeconds(i * 5).toString().replace("T", " ").replace("-", "").replace("+", ":"),		// <- not proud of this piece of code, but hey, it works ;)
+					generateRandomDoubleBetweenXandY(5, 20), 
+					generateRandomDoubleBetweenXandY(5, 20), 
+					generateRandomDoubleBetweenXandY(5, 20), 
+					generateRandomDoubleBetweenXandY(5, 20), 
+					Decimal.get(generateRandomDoubleBetweenXandY(0, 10000))
+					));
+		}
+		stopLoss = generateRandomDoubleBetweenXandY(6, 10);
+	}
+	
+	private static double generateRandomDoubleBetweenXandY(int lower, int upper) {
+		return (Math.random() * (upper - lower)) + lower;
 	}
 	
 	@Test
 	void callDefaultConstructorTest() throws IOException, InterruptedException {
 		LineChartCreator creator = new LineChartCreator();
-		creator.createLineGraphAndSaveFile();
+		creator.createLineGraphAndSaveFile(mockData, stopLoss);
 		Desktop.getDesktop().open(lineChart);
 		Thread.sleep(2000);
 		assertTrue(new File(LineChartCreator.LINECHART).exists());
