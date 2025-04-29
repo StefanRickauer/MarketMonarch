1cf31a0981d33a4c87f13d6194b20a5e406f81ac
Stefan Rickauer <stefanrick@gmx.de>
2025-04-29 20:35:40 +0200

    FinancialDataDto um die Methode getAllTrades() erweiteret, die eine List<TradeDto> zurückgibt. Testklasse entsprechend angepasst.
    Signed-off-by: Stefan Rickauer <stefanrick@gmx.de>

--- src/main/java/com/rickauer/marketmonarch/db/FinancialDataDao.java
+++ src/main/java/com/rickauer/marketmonarch/db/FinancialDataDao.java
@@ -1,6 +1,12 @@
 package com.rickauer.marketmonarch.db;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.rickauer.marketmonarch.configuration.DatabaseConnector;
+import com.rickauer.marketmonarch.db.data.TradeDto;
+import com.rickauer.marketmonarch.utils.StockUtils;
 import com.rickauer.marketmonarch.utils.Visitor;
 
 public final class FinancialDataDao extends DatabaseDao {
@@ -14,4 +20,37 @@ public final class FinancialDataDao extends DatabaseDao {
 		visitor.visit(this);
 	}
 
+	public List<TradeDto> getAllTrades() {
+		DatabaseConnector.INSTANCE.initializeDatabaseConnector();
+		FinancialDataDao db = new FinancialDataDao(DatabaseConnector.INSTANCE.getUrlFinancialData(), DatabaseConnector.INSTANCE.getUsername(), DatabaseConnector.INSTANCE.getPassword());
+		
+		String query = "SELECT * FROM trade";
+		
+		List<TradeDto> trades = new ArrayList<>();
+		TradeDto row = null;
+		
+		try (ResultSet allTrades = db.executeSqlQuery(query)) {
+			
+			while (allTrades.next()) {
+				row = new TradeDto();
+
+				row.setSymbol(allTrades.getString("symbol"));
+				row.setBuyOrderId(allTrades.getInt("buy_order_id"));
+				row.setSellOrderId(allTrades.getInt("sell_order_id"));
+				row.setEntryPrice(allTrades.getDouble("entry_price"));
+				row.setExitPrice(allTrades.getDouble("exit_price"));
+				row.setStopLoss(allTrades.getDouble("stop_loss"));
+				row.setQuantity(allTrades.getInt("quantity"));
+				row.setEntryTime(StockUtils.timestampToLocalDateTime(allTrades.getTimestamp("entry_time")));
+				row.setExitTime(StockUtils.timestampToLocalDateTime(allTrades.getTimestamp("exit_time")));
+				
+				trades.add(row);
+			}
+			
+			return trades;
+			
+		} catch (Exception e) {
+			throw new RuntimeException("Error executing query: '" + query + "'.", e);
+		}
+	}
 }
--- src/test/java/com/rickauer/marketmonarch/db/FinancialDataDaoTest.java
+++ src/test/java/com/rickauer/marketmonarch/db/FinancialDataDaoTest.java
@@ -4,23 +4,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import com.rickauer.marketmonarch.configuration.DatabaseConnector;
+import com.rickauer.marketmonarch.db.data.TradeDto;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class FinancialDataDaoTest {
 
+	private static final String id = "9999999";
+	private static final String symbol = "APPL";
+	private static final String buyId = "2";
+	private static final String sellId = "3";
+	private static final String entry = "15.0";
+	private static final String exit = "20.0";
+	private static final String quan = "100";
+	private static final String entryTime = "2025-04-23 21:16:35";
+	private static final String exitTime = "2025-04-28 21:16:35";
+	private static final String stopLoss = "12.0";
+	public static final String query = String.format("INSERT INTO trade VALUES(%s, '%s', %s, %s, %s, %s, %s, '%s', '%s', %s)", 
+			id, symbol, buyId, sellId, entry, exit, quan, entryTime, exitTime, stopLoss);
+	
 	@Test
 	void A_insertIntoDatabaseTest() {
 		DatabaseConnector.INSTANCE.initializeDatabaseConnector();
 		FinancialDataDao db = new FinancialDataDao(DatabaseConnector.INSTANCE.getUrlFinancialData(), DatabaseConnector.INSTANCE.getUsername(), DatabaseConnector.INSTANCE.getPassword());
 		
 		int rows = 0;
-		rows = db.executeSqlUpdate("INSERT INTO trade VALUES(9999999, 2, 3, 15.0, 20.0, 100, '2025-04-23 21:16:35', '2025-04-28 21:16:35', 12.0)");
+		rows = db.executeSqlUpdate(query);
 		
 		assertTrue(rows != 0);
 	}
@@ -28,7 +44,7 @@ public class FinancialDataDaoTest {
 	@Test
 	void B_queryDatabaseTest() {
 		DatabaseConnector.INSTANCE.initializeDatabaseConnector();
-		ApiKeyDao db = new ApiKeyDao(DatabaseConnector.INSTANCE.getUrlFinancialData(), DatabaseConnector.INSTANCE.getUsername(), DatabaseConnector.INSTANCE.getPassword());
+		FinancialDataDao db = new FinancialDataDao(DatabaseConnector.INSTANCE.getUrlFinancialData(), DatabaseConnector.INSTANCE.getUsername(), DatabaseConnector.INSTANCE.getPassword());
 		
 		try (ResultSet result = db.executeSqlQuery("SELECT entry_price FROM trade where id = 9999999")) {
 			
@@ -46,7 +62,31 @@ public class FinancialDataDaoTest {
 	}
 	
 	@Test
-	void C_deleteFromDatabaseTest() {
+	void C_getAllTrades() {
+		DatabaseConnector.INSTANCE.initializeDatabaseConnector();
+		FinancialDataDao db = new FinancialDataDao(DatabaseConnector.INSTANCE.getUrlFinancialData(), DatabaseConnector.INSTANCE.getUsername(), DatabaseConnector.INSTANCE.getPassword());
+		
+		List<TradeDto> trades = db.getAllTrades();
+		
+		for (TradeDto trade : trades) {
+			
+			if (trade.getId() != Integer.parseInt(id)) {
+				continue;		// skip other entries in case there is more data
+			}
+
+			assertEquals(symbol, trade.getSymbol());
+			assertEquals(Integer.parseInt(buyId), trade.getBuyOrderId());
+			assertEquals(Integer.parseInt(sellId), trade.getSellOrderId());
+			assertEquals(Double.parseDouble(entry), trade.getEntryPrice());
+			assertEquals(Double.parseDouble(exit), trade.getExitPrice());
+			assertEquals(Integer.parseInt(quan), trade.getQuantity());
+			assertEquals(LocalDateTime.parse(entryTime.replace(" ", "T")), trade.getEntryTime());
+			assertEquals(LocalDateTime.parse(exitTime.replace(" ", "T")), trade.getExitTime());
+		}
+	}
+	
+	@Test
+	void D_deleteFromDatabaseTest() {
 		DatabaseConnector.INSTANCE.initializeDatabaseConnector();
 		FinancialDataDao db = new FinancialDataDao(DatabaseConnector.INSTANCE.getUrlFinancialData(), DatabaseConnector.INSTANCE.getUsername(), DatabaseConnector.INSTANCE.getPassword());
 		
