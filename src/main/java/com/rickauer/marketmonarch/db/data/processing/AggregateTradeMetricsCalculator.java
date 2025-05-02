3e25e5868717a038e5af43c9e3f6a76ea4e92ed4
Stefan Rickauer <stefanrick@gmx.de>
2025-05-02 21:24:21 +0200

    Klasse zur Berechnung aggregierter Trading-Metriken hinzugefügt, Testklasse erstellt und Testklasse zu Test-Suite hinzugefügt.
    Signed-off-by: Stefan Rickauer <stefanrick@gmx.de>

--- src/main/java/com/rickauer/marketmonarch/db/data/processing/AggregateTradeMetricsCalculator.java
+++ src/main/java/com/rickauer/marketmonarch/db/data/processing/AggregateTradeMetricsCalculator.java
@@ -1,5 +1,67 @@
 package com.rickauer.marketmonarch.db.data.processing;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.rickauer.marketmonarch.db.data.TradeDto;
+
 public class AggregateTradeMetricsCalculator {
-	; // add code here ;)
+
+	public static double calculateAverageProfitAbsolute(List<TradeDto> trades) {
+		return trades.stream()
+				.mapToDouble(trade -> SingleTradeMetricsCalculator.calculateProfitAbsolute(trade))
+				.average()
+				.orElse(0.0);
+	}
+	
+	public static double calculateSharpeRatio(List<TradeDto> trades) {
+		return calculateSharpeRatio(trades, 0.0);
+	}
+	
+	public static double calculateSharpeRatio(List<TradeDto> trades, double riskFreeRate) {
+		List<Double> returns = trades.stream()
+				.map(trade -> SingleTradeMetricsCalculator.calculateProfitDecimal(trade))
+				.toList();
+		
+		double averageReturnDecimal = returns.stream()
+				.mapToDouble(Double::doubleValue)
+				.average()
+				.orElse(0.0);
+		double standardDeviation = calculateStandardDeviation(returns);
+		
+		return ( averageReturnDecimal - riskFreeRate) / standardDeviation;
+	}
+	
+	public static double calculateStandardDeviation(List<Double> returns) {
+	    double mean = returns.stream()
+	    		.mapToDouble(Double::doubleValue)
+	    		.average()
+	    		.orElse(0.0);
+	    double variance = returns.stream()
+	    		.mapToDouble(r -> Math.pow(r - mean, 2))
+	    		.average().orElse(0.0);
+	    return Math.sqrt(variance);
+	}
+	
+	public static double calculateProfitFactor(List<TradeDto> trades) {
+		return calculateAllProfitsAbsolute(trades) / Math.abs(calculateAllLossesAbsolute(trades));
+	}
+	
+	public static double calculateAllProfitsAbsolute(List<TradeDto> trades) {
+		return trades.stream()
+				.map(trade -> SingleTradeMetricsCalculator.calculateProfitAbsolute(trade))
+				.filter(profit -> profit > 0)
+				.mapToDouble(Double::doubleValue)
+				.sum();
+	}
+	
+	public static double calculateAllLossesAbsolute(List<TradeDto> trades) {
+		double allLosses = trades.stream()
+				.map(trade -> SingleTradeMetricsCalculator.calculateProfitAbsolute(trade))
+				.filter(profit -> profit <= 0)
+				.mapToDouble(Double::doubleValue)
+				.sum();
+		
+		return allLosses == 0 ? 1.0 : allLosses; 
+	}
 }
---
+++ src/test/java/com/rickauer/marketmonarch/db/data/processing/AggregateTradeMetricsCalculatorTest.java
@@ -0,0 +1,88 @@
+package com.rickauer.marketmonarch.db.data.processing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.rickauer.marketmonarch.db.data.TradeDto;
+
+public class AggregateTradeMetricsCalculatorTest {
+
+	private static List<TradeDto> testData;
+	
+	@BeforeAll
+	static void initializeTestData() {
+		testData = new ArrayList<>();
+		TradeDto firstTrade = new TradeDto();
+		firstTrade.setEntryPrice(100.0);
+		firstTrade.setExitPrice(105.0);
+		firstTrade.setQuantity(10);
+		testData.add(firstTrade);
+		
+		TradeDto secondTrade = new TradeDto();
+		secondTrade.setEntryPrice(200.0);
+		secondTrade.setExitPrice(190.0);
+		secondTrade.setQuantity(5);
+		testData.add(secondTrade);
+
+		TradeDto thirdTrade = new TradeDto();
+		thirdTrade.setEntryPrice(150.0);
+		thirdTrade.setExitPrice(165.0);
+		thirdTrade.setQuantity(8);
+		testData.add(thirdTrade);
+
+		TradeDto fourthTrade = new TradeDto();
+		fourthTrade.setEntryPrice(300.0);
+		fourthTrade.setExitPrice(285.0);
+		fourthTrade.setQuantity(4);
+		testData.add(fourthTrade);
+	}
+	
+	@Test
+	void calculateAverageProfitTest() {
+		double avgProfitAbsolute = AggregateTradeMetricsCalculator.calculateAverageProfitAbsolute(testData);
+		assertEquals(15.0, avgProfitAbsolute);
+	}
+	
+	@Test
+	void calculateSharpeRatioTest() {
+		double sharpeRatio = AggregateTradeMetricsCalculator.calculateSharpeRatio(testData, 0);
+		assertEquals("0.1925", truncateUsingBigDecimal(sharpeRatio));
+	}
+
+	@Test
+	void calculateStandardDeviationTest() {
+		List<Double> test = List.of(0.05, -0.05, 0.1, -0.05);
+		double stdDev = AggregateTradeMetricsCalculator.calculateStandardDeviation(test);
+		assertEquals("0.0650", truncateUsingBigDecimal(stdDev));
+	}
+	
+	@Test
+	void calculateProfitFactorTest() {
+		double profitFactor = AggregateTradeMetricsCalculator.calculateProfitFactor(testData);
+		assertEquals("1.5455", truncateUsingBigDecimal(profitFactor));
+	}
+	
+	@Test
+	void calculateAllProfitsTest() {
+		double allProfits = AggregateTradeMetricsCalculator.calculateAllProfitsAbsolute(testData);
+		assertEquals(170.0, allProfits);
+	}
+	
+	@Test
+	void calculateAllLossesAbsoluteTest() {
+		double allLosses = AggregateTradeMetricsCalculator.calculateAllLossesAbsolute(testData);
+		assertEquals(-110.0, allLosses);
+	}
+	
+	private static String truncateUsingBigDecimal(double value) {
+		BigDecimal valueAsBD = new BigDecimal(value).setScale(4, RoundingMode.HALF_EVEN);
+		return valueAsBD.toString();
+	}
+}
