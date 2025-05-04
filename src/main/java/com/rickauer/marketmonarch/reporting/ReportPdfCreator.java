685187ea721d207b2c9a1eeda2f784c575bdcea8
Stefan Rickauer <stefanrick@gmx.de>
2025-05-04 20:03:55 +0200

    Implementierungsanweisungen hinzugefügt.
    Signed-off-by: Stefan Rickauer <stefanrick@gmx.de>

--- src/main/java/com/rickauer/marketmonarch/reporting/ReportPdfCreator.java
+++ src/main/java/com/rickauer/marketmonarch/reporting/ReportPdfCreator.java
@@ -1,5 +1,10 @@
 package com.rickauer.marketmonarch.reporting;
 
 public final class ReportPdfCreator {
-	// Create pdf with line chart. PDF file will be send to me via email
+	;
+	// Create PDF with line chart. PDF file will be send to me via email
+	// Divide data into 
+	//	- trading data
+	//  - trading specific metrics		->	SingleTradeMetricsCalculator
+	//  - accumulated trading metrics	-> 	AggregateTradeMetricsCalculator
 }
