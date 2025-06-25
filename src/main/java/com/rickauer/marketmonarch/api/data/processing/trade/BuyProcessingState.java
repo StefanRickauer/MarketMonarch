7117d5ca58b452653d5665f8de8713d9cb2f8e80
Stefan Rickauer <stefanrick@gmx.de>
2025-06-25 19:02:40 +0200

    Debug-Messages gelöscht.
    Signed-off-by: Stefan Rickauer <stefanrick@gmx.de>

--- src/main/java/com/rickauer/marketmonarch/api/data/processing/trade/BuyProcessingState.java
+++ src/main/java/com/rickauer/marketmonarch/api/data/processing/trade/BuyProcessingState.java
@@ -39,11 +39,8 @@ public class BuyProcessingState extends TradeState {
 		
 		if (_context.getController().getPortNumber() == TradingConstants.REAL_MONEY_TRADING_PORT_NUMBER) {
 			_context.setQuantity(StockUtils.calculateQuantity(MarketMonarch._preTradeContext.getTotalCash(), MarketMonarch._tradingContext.getEntryPrice()));	
-			; // löschen, wenn es funktioniert
-			_buyOrderLogger.debug("This should be executed during real money sessions.");
 		} else {
 			_context.setQuantity(StockUtils.calculateQuantity(1080.0, MarketMonarch._tradingContext.getEntryPrice()));	
-			_buyOrderLogger.debug("This should not be executed during real money sessions.");
 		}
 		
 		Decimal quantity = _context.getQuantity(); 
